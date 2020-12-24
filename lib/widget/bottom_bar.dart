d1b4c12a581fc1f7fae4320992570369cea7860d
wood5186 <60569974+wood5186@users.noreply.github.com>
2020-12-24 23:44:51 +0900

    add_bottomTabBar

---
+++ lib/widget/bottom_bar.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class Bottom extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 55,
+      color: Colors.transparent,
+      child: TabBar(
+        labelColor: Colors.white,
+        unselectedLabelColor: Colors.black38,
+        indicatorSize: TabBarIndicatorSize.tab,
+        indicatorColor: Colors.transparent,
+        indicator: BoxDecoration(
+          border: Border.all(color: Colors.white, width: 7),
+          gradient:
+              LinearGradient(colors: [Colors.redAccent, Colors.orangeAccent]),
+          borderRadius: BorderRadius.circular(30),
+          //color: Colors.redAccent,
+        ),
+        tabs: [
+          Tab(
+            icon: Icon(
+              Icons.map_outlined,
+              size: 20,
+            ),
+            /*child: Text(
+              '지도',
+              style: TextStyle(fontSize: 10),
+            ),*/
+          ),
+          Tab(
+            icon: Icon(
+              Icons.search_outlined,
+              size: 20,
+            ),
+            /*child: Text(
+              '주변',
+              style: TextStyle(fontSize: 10),
+            ),*/
+          ),
+          Tab(
+            icon: Icon(
+              Icons.star_border,
+              size: 20,
+            ),
+            /*child: Text(
+              '즐겨찾기',
+              style: TextStyle(fontSize: 10),
+            ),*/
+          ),
+          Tab(
+            icon: Icon(
+              Icons.arrow_upward,
+              size: 20,
+              color: Colors.blueAccent,
+            ),
+            /*child: Text(
+              '길찾기',
+              style: TextStyle(fontSize: 11, color: Colors.blue),
+            ),*/
+          ),
+        ],
+      ),
+    );
+  }
+}

2106a426f1785821287f19429bf85853b1e62130
wood5186 <wood5186@naver.com>
2020-12-26 06:07:33 +0900

    add_map

--- lib/screen/map_screen.dart
+++ lib/screen/map_screen.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:geolocator/geolocator.dart';
 
+/*class MyMap extends StatefulWidget {
+  _MyMap createState() => _MyMap();
+}*/
+
+// ignore: must_be_immutable
 class MyMap extends StatelessWidget {
   GoogleMapController mapController;
 
-  final LatLng _center = const LatLng(45.521563, -122.677433);
+  double lat = 0.00;
+  double lon = 0.00;
+  LatLng seoul = LatLng(126.734086, 127.269311);
+  
+  Future<Position> getCurrentLocation() async {
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high);
+    lat = position.latitude;
+    lon = position.longitude;
+
+    return position;
+  }
 
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
@@ -12,10 +30,27 @@ class MyMap extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    /*return Center(
         child: GoogleMap(
       onMapCreated: _onMapCreated,
-      initialCameraPosition: CameraPosition(target: _center, zoom: 11.0),
-    ));
+      initialCameraPosition: CameraPosition(target: LatLng(lat, lon), zoom: 8.0),
+    ));*/
+    return getCurrentLocation() != null
+        ? Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: LatLng(lat, lon), zoom: 8),
+            ),
+          )
+        : Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: GoogleMap(
+              initialCameraPosition:
+                  CameraPosition(target: seoul, zoom: 8),
+            ),
+          );
   }
 }
